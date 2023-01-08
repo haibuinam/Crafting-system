45c8ee7180e0f424b1906105cfab280e5a5a2091
BeLikeLaptop <Buinamhai421998@gmail.com>
2023-01-08 14:08:58 +0700

    add attributeSet

---
+++ MyProject/Source/MyProject/Public/GAS/Attribute/AttributeSetBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AttributeSetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+
+};
