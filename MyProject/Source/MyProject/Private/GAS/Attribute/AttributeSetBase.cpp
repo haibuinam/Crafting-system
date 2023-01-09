4c580711996e6f177f0af92bc06c034c8745fa06
BeLikeLaptop <Buinamhai421998@gmail.com>
2023-01-09 22:53:43 +0700

    update getter/seter base on attributeSet

--- MyProject/Source/MyProject/Private/GAS/Attribute/AttributeSetBase.cpp
+++ MyProject/Source/MyProject/Private/GAS/Attribute/AttributeSetBase.cpp
@@ -3,3 +3,37 @@
 
 #include "GAS/Attribute/AttributeSetBase.h"
 
+void UAttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, Health, OldHealth);
+}
+
+void UAttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, MaxHealth, OldMaxHealth);
+}
+
+void UAttributeSetBase::OnRep_HealthRegenRate(const FGameplayAttributeData& OldHealthRegenRate)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, HealthRegenRate, OldHealthRegenRate);
+}
+
+void UAttributeSetBase::OnRep_Armor(const FGameplayAttributeData& OldArmor)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, Armor, OldArmor);
+}
+
+void UAttributeSetBase::OnRep_Level(const FGameplayAttributeData& OldLevel)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, Level, OldLevel);
+}
+
+void UAttributeSetBase::OnRep_XP(const FGameplayAttributeData& OldXP)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, XP, OldXP);
+}
+
+void UAttributeSetBase::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+
+}
--- MyProject/Source/MyProject/Public/GAS/Attribute/AttributeSetBase.h
+++ MyProject/Source/MyProject/Public/GAS/Attribute/AttributeSetBase.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSetBase.generated.h"
+
+//macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,5 +22,55 @@ class MYPROJECT_API UAttributeSetBase : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Health")
+	FGameplayAttributeData Health;
+ 	ATTRIBUTE_ACCESSORS(UAttributeSetBase, Health)
 
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Health")
+	FGameplayAttributeData MaxHealth;
+ 	ATTRIBUTE_ACCESSORS(UAttributeSetBase, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Health")
+	FGameplayAttributeData HealthRegenRate;
+ 	ATTRIBUTE_ACCESSORS(UAttributeSetBase, HealthRegenRate)
+
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Damage")
+	FGameplayAttributeData Damage;
+ 	ATTRIBUTE_ACCESSORS(UAttributeSetBase, Damage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Armor")
+	FGameplayAttributeData Armor;
+ 	ATTRIBUTE_ACCESSORS(UAttributeSetBase, Armor)
+
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_Level")
+	FGameplayAttributeData Level;
+	ATTRIBUTE_ACCESSORS(UAttributeSetBase, Level)
+
+	UPROPERTY(BlueprintReadOnly, Category = "AttributeSetBase_XP")
+	FGameplayAttributeData XP;
+	ATTRIBUTE_ACCESSORS(UAttributeSetBase, XP)
+
+public:
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	UFUNCTION()
+	virtual void OnRep_HealthRegenRate(const FGameplayAttributeData& OldHealthRegenRate);
+
+	UFUNCTION()
+	virtual void OnRep_Armor(const FGameplayAttributeData& OldArmor);
+
+	UFUNCTION()
+	virtual void OnRep_Level(const FGameplayAttributeData& OldLevel);
+
+	UFUNCTION()
+	virtual void OnRep_XP(const FGameplayAttributeData& OldXP);
+
+public:
+	//to clamp new value (stl::clamp c++ 20): Clamp(Health, 0, MaxHealth)
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 };
